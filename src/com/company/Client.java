a9fe7b599314ce947431150a7912a03275e4dfba
vitamin7777777 <rlaalsdn8@gmail.com>
2021-07-27 00:26:13 +0900

    Server , Client Training Test add

---
+++ src/com/company/Client.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String server = args[0];
+        int port = Integer.parseInt(args[1]);
+        //클라이언트 소켓 객체 생성 ( 서버에 연결 요청 )
+        Socket clientSocket = new Socket(server, port);
+        //소켓 입력 스트림 객체 생성
+        InputStream Istream = clientSocket.getInputStream();
+        //스트림 변환
+        DataInputStream DIstream = new DataInputStream(Istream);
+
+        for (int i = 1; i <= 5; i++){
+            //데이터 수신
+            int imsg = DIstream.readInt();
+            System.out.println("서버로부터 받은 데이터 : " + imsg);
+        }
+        // 소켓 닫음
+        clientSocket.close();
+    }
+
+}
---
+++ src/com/company/Server.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Server {
+    public static void main(String args[]) throws IOException {
+        int port = Integer.parseInt(args[0]) ;
+        //서버 소켓 객체 생성
+        ServerSocket serverSocket = new ServerSocket(port) ;
+
+        System.out.println("서버 실행중 !");
+        while (true){
+            //연결수락 , 클라이언트 소켓 객체 생성
+            Socket clientSocket = serverSocket.accept();
+            //소켓 출력 스트림 생성
+            OutputStream Ostream = clientSocket.getOutputStream();
+            //스트림 변환
+            DataOutputStream DOstream = new DataOutputStream(Ostream);
+
+            for (int imsg = 1; imsg <= 5; imsg++)
+                //데이터 전송
+                DOstream.writeInt(imsg);
+                //소켓 닫기
+            clientSocket.close();
+        }
+    }
+}
